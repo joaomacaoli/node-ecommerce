415db776284f8815a5cf5b676df0bd7902b0b4ad
João Marcelo <joaomarcelo.co@hotmail.com>
2024-05-09 12:31:33 -0300

    set users models properties

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -5,14 +5,50 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  image: {
+  email: {
     type: String,
     required: true,
   },
-  countInStock: {
-    type: Number,
+  passwordHash: {
+    type: String,
     required: true,
   },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  street: {
+    type: String,
+    default: '',
+  },
+  apartment: {
+    type: String,
+    default: '',
+  },
+  zip: {
+    type: String,
+    default: '',
+  },
+  city: {
+    type: String,
+    default: '',
+  },
+  country: {
+    type: String,
+    default: '',
+  },
+})
+
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
 })
 
 const UserModel = model('User', userSchema)
