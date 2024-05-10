073ea2edc8a815412215cd4425645cbd62c346b4
João Marcelo <joaomarcelo.co@hotmail.com>
2024-05-10 20:34:44 -0300

    create authentication and error handling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import routes from "./routes/index.js";
-import mongo from "./db/mongo.js";
+import routes from './routes/index.js'
+import './db/mongo.js'
+import authJwt from './helpers/jwt.js'
+import errorHandler from './helpers/error-handler.js'
 
 const app = express()
 
 app.use(express.json())
-app.use(routes);
+app.use(authJwt())
+app.use(errorHandler)
+app.use(routes)
 
 const port = process.env.PORT
 const message = `You are logged!`
